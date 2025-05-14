82824a4d4c68265b3df99ecc141bff3e749c89ed
Mateus Lidonis Blanco <mateus.lidonis@gmail.com>
2025-05-13 23:17:46 -0300

    item persistence

--- supermercado.js
+++ supermercado.js
@@ -1,44 +1,27 @@
 var items = [];
 
+let dadosSalvos = localStorage.getItem('listaProdutos')
+if (dadosSalvos) {
+    items = JSON.parse(dadosSalvos)
+    atualizarLista()
+}
+
 document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
     var nomeProduto = document.querySelector('input[name=nome_produto]')
     var precoProduto = document.querySelector('input[name=price]')
 
-    items.push({
-        nome: nomeProduto.value,
-        valor: precoProduto.value
-    })
+    if (nomeProduto.value && precoProduto.value){
+        items.push({
+            nome: nomeProduto.value,
+            valor: precoProduto.value
+        })
 
-    /*
-        <div class="lista-produto-single">
-            <h3>Chocolate</h3>
-            <h3 class="price-produto"><span>R$15,00</span></h3>
-        </div>
-    */
+        nomeProduto.value = ''
+        precoProduto.value = ''
 
-    let listaProdutos = document.querySelector('.lista-produtos')
-    let soma = 0
-
-    listaProdutos.innerHTML = ''
-    items.map(function(val){
-        soma += parseFloat(val.valor)
-        listaProdutos.innerHTML+=`
-        <div class="lista-produto-single">
-            <h3>`+val.nome+`</h3>
-            <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
-            <button name="excluir-produto">Excluir</button>
-        </div>
-        `
-    })
-
-    soma = soma.toFixed(2)
-    nomeProduto.value = ''
-    precoProduto.value = ''
-
-    let elementoSoma = document.querySelector('.soma-produto h1')
-    elementoSoma.innerHTML = 'R$' + soma
-
+        atualizarLista()
+    }
 })
 
 document.querySelector('.lista-produtos')
@@ -46,38 +29,46 @@ document.querySelector('.lista-produtos')
     if (e.target && e.target.getAttribute('name') === 'excluir-produto') {
 
         const produtoDiv = e.target.closest('.lista-produto-single');
-        const nomeProduto = produtoDiv.querySelector('h3').innerText.trim();
-
-        // encontrar o índice real baseado no nome
         const index = Array.from(document.querySelectorAll('.lista-produto-single')).indexOf(produtoDiv);
 
         if (index !== -1) {
-            items.splice(index, 1); // remove 1 item no índice encontrado
+            items.splice(index, 1);
+            atualizarLista()
         }
-        
-        let listaProdutos = document.querySelector('.lista-produtos');
-        let soma = 0;
-        listaProdutos.innerHTML = '';
-        items.map(function(val){
-            soma += parseFloat(val.valor);
-            listaProdutos.innerHTML += `
-            <div class="lista-produto-single">
-                <h3>`+val.nome+`</h3>
-                <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
-                <button class="excluir-produto" name="excluir-produto">Excluir</button>
-            </div>
-            `;
-        });
-
-        soma = soma.toFixed(2);
-        let elementoSoma = document.querySelector('.soma-produto h1');
-        elementoSoma.innerHTML = 'R$' + soma;
     }
 });
 
 document.querySelector('button[name=limpar]')
 .addEventListener('click',()=>{
     items = []
-    document.querySelector('.lista-produtos').innerHTML = ''
-    document.querySelector('.soma-produto h1').innerHTML = 'Total: R$ 0'
+    atualizarLista()
 })
+
+function atualizarLista(){
+    /*
+        <div class="lista-produto-single">
+            <h3>Chocolate</h3>
+            <h3 class="price-produto"><span>R$15,00</span></h3>
+        </div>
+    */
+
+        let listaProdutos = document.querySelector('.lista-produtos')
+        let soma = 0
+    
+        listaProdutos.innerHTML = ''
+        items.map(function(val){
+            soma += parseFloat(val.valor)
+            listaProdutos.innerHTML += `
+            <div class="lista-produto-single">
+                <h3>${val.nome}</h3>
+                <h3 class="price-produto"><span>R$${parseFloat(val.valor).toFixed(2)}</span></h3>
+                <button name="excluir-produto">Excluir</button>
+            </div>
+            `
+        })
+    
+        soma = soma.toFixed(2)
+        document.querySelector('.soma-produto h1').innerHTML = 'R$' + soma;
+
+        localStorage.setItem('listaProdutos', JSON.stringify(items))
+}
